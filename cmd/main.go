41c2f984e25f936e065515c9742f909b8206eca1
budougumi0617 <budougumi0617@gmail.com>
2019-09-15 21:19:57 +0900

    :sparkles: add skeleton

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"log"
+	"os"
+
+	"github.com/budougumi0617/dkl"
+)
 
 func main() {
-	fmt.Println("vim-go")
+	log.SetFlags(0)
+	err := dkl.Run(os.Args, os.Stdin, os.Stdout, os.Stderr)
+	if err != nil && err != flag.ErrHelp {
+		log.Println(err)
+		exitCode := 1
+		if ecoder, ok := err.(interface{ ExitCode() int }); ok {
+			exitCode = ecoder.ExitCode()
+		}
+		os.Exit(exitCode)
+	}
 }
---
+++ dkl.go
@@ -0,0 +1,37 @@
+package dkl
+
+import (
+	"flag"
+	"fmt"
+	"io"
+)
+
+const (
+	version = "0.0.0"
+)
+
+// Run is entry point.
+func Run(args []string, inStream io.Reader, outStream, errStream io.Writer) error {
+	var v bool
+	cmdName := args[0]
+	vdesc := "Print version information and quit."
+	flags := flag.NewFlagSet(cmdName, flag.ContinueOnError)
+	flags.SetOutput(errStream)
+
+	flags.BoolVar(&v, "version", false, vdesc)
+	flags.BoolVar(&v, "v", false, vdesc)
+
+	if err := flags.Parse(args[1:]); err != nil {
+		return err
+	}
+
+	// バージョン情報の表示
+	if v {
+		fmt.Fprintf(errStream, "%s version %s\n", cmdName, version)
+		return nil
+	}
+
+	fmt.Fprint(outStream, "now implementing...\n")
+
+	return nil
+}
