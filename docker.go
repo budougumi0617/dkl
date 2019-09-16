9c8e5e4e4364776a6fbb89b0d78e844e4753b6da
budougumi0617 <budougumi0617@gmail.com>
2019-09-16 15:38:02 +0900

    :sparkles: list containers

--- docker.go
+++ docker.go
@@ -3,9 +3,11 @@ package dkl
 import (
 	"context"
 	"fmt"
+	"strings"
 
 	"github.com/docker/docker/api/types"
 	"github.com/docker/docker/client"
+	"github.com/manifoldco/promptui"
 )
 
 func show() {
@@ -19,7 +21,42 @@ func show() {
 		panic(err)
 	}
 
-	for _, container := range containers {
-		fmt.Printf("%s %s\n", container.ID[:10], container.Image)
+	templates := &promptui.SelectTemplates{
+		Label:    "{{ . }}?",
+		Active:   "\U0001F40B {{ index .Names 0 | cyan }} ({{ .Image | red }}, {{ .ImageID }})",
+		Inactive: "   {{ index .Names 0 | cyan }} ({{ .Image | red }}, {{ .ImageID  }})",
+		Selected: "\U0001F336 {{ index .Names 0 | red }}",
+		Details: `
+--------- Image ----------
+{{ "Name:" | faint }}	{{ printf "%q" .Names }}
+{{ "Image:" | faint }}	{{ .Image }}
+{{ "ImageID:" | faint }}	{{ .ImageID }}`,
 	}
+
+	searcher := func(input string, index int) bool {
+		container := containers[index]
+		n := strings.Join(container.Names, "")
+		ni := n + container.Image
+		lni := strings.ReplaceAll(strings.ToLower(ni), " ", "")
+		input = strings.ReplaceAll(strings.ToLower(input), " ", "")
+
+		return strings.Contains(lni, input)
+	}
+
+	prompt := promptui.Select{
+		Label:     "Docker running...",
+		Items:     containers,
+		Templates: templates,
+		Size:      10,
+		Searcher:  searcher,
+	}
+
+	i, _, err := prompt.Run()
+
+	if err != nil {
+		fmt.Printf("Prompt failed %v\n", err)
+		return
+	}
+
+	fmt.Printf("containers[%d] = %+v\n", i, containers[i])
 }
--- go.mod
+++ go.mod
@@ -6,17 +6,20 @@ require (
 	github.com/Azure/go-ansiterm v0.0.0-20170929234023-d6e3b3328b78 // indirect
 	github.com/Microsoft/go-winio v0.4.14 // indirect
 	github.com/Nvveen/Gotty v0.0.0-20120604004816-cd527374f1e5 // indirect
+	github.com/alecthomas/units v0.0.0-20190910110746-680d30ca3117 // indirect
 	github.com/docker/distribution v2.7.1+incompatible // indirect
 	github.com/docker/docker v1.4.2-0.20181026133427-e238713cc9da
 	github.com/docker/go-connections v0.4.0 // indirect
 	github.com/docker/go-units v0.4.0 // indirect
 	github.com/gogo/protobuf v1.3.0 // indirect
 	github.com/gorilla/mux v1.7.3 // indirect
+	github.com/manifoldco/promptui v0.3.2
 	github.com/opencontainers/go-digest v1.0.0-rc1 // indirect
 	github.com/opencontainers/image-spec v1.0.1 // indirect
 	github.com/sirupsen/logrus v1.4.2 // indirect
 	golang.org/x/net v0.0.0-20190912160710-24e19bdeb0f2 // indirect
 	golang.org/x/time v0.0.0-20190308202827-9d24e82272b4 // indirect
 	google.golang.org/grpc v1.23.1 // indirect
+	gopkg.in/alecthomas/kingpin.v3-unstable v3.0.0-20180810215634-df19058c872c // indirect
 	gotest.tools v2.2.0+incompatible // indirect
 )
